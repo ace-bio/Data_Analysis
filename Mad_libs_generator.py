ac814706ea214a41b0fd8d9919db123065be74d1
ace_bio <emmaustin20@gmail.com>
2023-12-28 23:15:45 +0100

    Latest

---
+++ Mad_libs_generator.py
@@ -0,0 +1,14 @@
+#Simple Mad Libs Generator Game!
+
+#prompt the user to enter a noun
+noun1 = input("Please enter a noun")
+# prompt the user to enter an adjective
+adjective = input("Please enter an adjective")
+# prompt the player for another noun
+noun2 = input("Please enter another noun: ")
+
+#print mad libs sentence
+
+print(noun1+ " are faster than the wind")
+print("Ducks are " + adjective)
+print("Dont run with a " + noun2)
